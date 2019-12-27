fc09c1cf148248f427e2e7b92f179986e0c835b5
quxiangyu <quxiangyu116@bytedance.com>
2019-12-27 16:57:20 +0800

    初始化lib

---
+++ Example/Pods/Target Support Files/Pods-XYUIKitTreeManager_Tests/Pods-XYUIKitTreeManager_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XYUIKitTreeManager_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_XYUIKitTreeManager_Tests
+@end
---
+++ Example/Pods/Target Support Files/XYUIKitTreeManager/XYUIKitTreeManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XYUIKitTreeManager : NSObject
+@end
+@implementation PodsDummy_XYUIKitTreeManager
+@end
